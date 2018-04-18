782197fa7fc81eb7d1d9c66b20f9270530a2bae3
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-04-18 14:31:32 +0200

    Add node

--- calculator/src/main/scala/calculator/parser/Trees.scala
+++ calculator/src/main/scala/calculator/parser/Trees.scala
@@ -13,9 +13,20 @@ object Trees {
   /** lhs: left hand side, rhs: right hand side */
   case class Assign(ident: Identifier, value: ExprTree) extends ExprTree
   case class Plus(lhs: ExprTree, rhs: ExprTree) extends ExprTree
-  ???
+  case class Minus(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Mult(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Div(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Modulo(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Power(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Gcd(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Fact(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Sqrt(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+
 
   /** Leaves Expression Trees */
+  case class Empty() extends ExprTree {
+    override def toString: String = "Empty('')"
+  }
   case class NumLit(value: String) extends ExprTree
   case class Identifier(value: String) extends ExprTree {
     override def toString: String = "Identifier('" + value + "')"
