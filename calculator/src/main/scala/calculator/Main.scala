0f9fb01c37ca2f992899a5ae638bc177733bc72e
Viking & Robot <vikingandrobot@gmail.com>
2018-04-23 19:12:40 +0200

    Usage function

--- calculator/src/main/scala/calculator/Main.scala
+++ calculator/src/main/scala/calculator/Main.scala
@@ -19,5 +19,42 @@ object Main {
     }
   }
 
-  def usage() = ???
+  def usage() : Unit = {
+    println("======================================================")
+    println("Calculator usage")
+    println("======================================================")
+    println("Arithmetic operations:")
+    println("   + : Addition, for example: 1 + 2")
+    println("   - : Subtraction, for example: 2 - 1")
+    println("   * : Multiplication, for example: 2 * 4")
+    println("   / : Division, for example: 4 / 2")
+    println("")
+    println("Other operations:")
+    println("   % : Modulo, for example: 7 % 3")
+    println("   ^ : Power, for example: 2^4")
+    println("   ! : Factorial, for example: 5!")
+    println("   - : Unary negative, for example:")
+    println("       -1")
+    println("       -(2 + 1)")
+    println("       -x")
+    println("       -gcd(3, 15)")
+    println("       -sqrt(5)")
+    println("       -3 + 2")
+    println("       -(-(-1))")
+    println("")
+    println("Memory:")
+    println("   = : Save value in memory variable, for example: x = 3")
+    println("       To retrieve the value, type in the name of the saved variable.")
+    println("       For example: 2 + x (will evaluate to 2 + 3)")
+    println("")
+    println("Parenthesis:")
+    println("   You can use parenthesis, for example: 6 * (2 + 3)")
+    println("")
+    println("Functions:")
+    println("   gcd(x, y) : Greatest common divisor, for example: gcd(4, 8)")
+    println("   sqrt(x, y) : Square root, for example: sqrt(4)")
+    println("")
+    println("Notes:")
+    println("   This calculator doesn't support decimal numbers in calculations. Sorry.")
+  }
 }
