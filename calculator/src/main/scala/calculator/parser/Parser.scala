a533f3cd51f68ef6a841856f94c2f39b0b8e4af4
Viking & Robot <vikingandrobot@gmail.com>
2018-04-24 19:48:46 +0200

    Commenting.

--- calculator/src/main/scala/calculator/parser/Parser.scala
+++ calculator/src/main/scala/calculator/parser/Parser.scala
@@ -106,7 +106,7 @@ class Parser(source:Source) extends Lexer(source:Source) {
   }
 
   /**
-    * Parses the mutliplication and division operations.
+    * Parses the multiplication and division operations.
     * This method calls the parseModulo method to compute the left tree and
     * then places it in a Mult or Div tree on the left. It then calls the parseModulo
     * method again to parse the right tree.
@@ -127,6 +127,13 @@ class Parser(source:Source) extends Lexer(source:Source) {
     e
   }
 
+  /**
+    * Parse the modulo operator.
+    * This method calls the parsePower method to find the left tree and
+    * then calls the parsePower method again to find the right tree.
+    *
+    * @return an ExprTree representing the calculation
+    */
   private def parseModulo: ExprTree = {
     var e = parsePower
     while (currentToken.info == MODULO) {
@@ -136,6 +143,13 @@ class Parser(source:Source) extends Lexer(source:Source) {
     e
   }
 
+  /**
+    * Parses the power operation.
+    * This method then calls the parseFactorial method to find the left tree and
+    * the right tree.
+    *
+    * @return an ExprTree representing the calculation
+    */
   private def parsePower: ExprTree = {
     var e = parseFactorial
     while (currentToken.info == POWER) {
@@ -145,6 +159,13 @@ class Parser(source:Source) extends Lexer(source:Source) {
     e
   }
 
+  /**
+    * Parse the factorail operator.
+    * This method then calls the parseSimpleExpr to parse the simple expressions
+    * in the calculation.
+    *
+    * @return an ExprTree representing the calculation
+    */
   private def parseFactorial: ExprTree = {
     var e = parseSimpleExpr
     while (currentToken.info == FACT) {
@@ -154,16 +175,31 @@ class Parser(source:Source) extends Lexer(source:Source) {
     e
   }
 
+  /**
+    * Parses the simple expressions.
+    * This method begins by detecting a minus unary negative operator
+    * (this is useful for negative number and negative operation such as
+    * -sqrt(4), -(2 + 3) etc.).
+    *
+    * Then it case matches the current token to call the correct parsing method.
+    *
+    * It then returns the result of said correct parsing method, wrapping the tree
+    * in a UnaryMinus object if a minus sign was detected before.
+    * @return
+    */
   private def parseSimpleExpr: ExprTree = {
+
+    /** Detect the presence of a minus sign **/
     var isMinus = false
     if (currentToken.info == MINUS) {
       eat(MINUS)
       isMinus = true
     }
 
+    /** The tree we will be returning **/
     var e : ExprTree = null
 
-    // Here you want to match simple expressions such as NUM(value) and parse them (for example with the parseExprTreeToken method).
+    /** Parse each possible token */
     currentToken.info match {
       case LPAREN => e = parseParenthesis // Parenthesis
       case VIRG => e = parseVirgule // Virgule
@@ -175,55 +211,85 @@ class Parser(source:Source) extends Lexer(source:Source) {
       case _    => fatalError("Invalid token " + currentToken.toString)
     }
 
+    /** Wrap in a UnaryMinus if necessary **/
     if (isMinus) {
       e = UnaryMinus(e)
     }
     e
   }
 
-  private def parseExprTreeToken[T <: ExprTree](retTree: T): ExprTree = {
-    readToken
-    retTree
-  }
-
+  /**
+    * Parses a numeric literal.
+    * @param value the numeric literal String representation
+    * @return a NumLit representing the numeric literal
+    */
   private def parseNum(value: String): ExprTree = {
     eat(NUM(value))
     val e = NumLit(value)
     e
   }
 
+  /**
+    * Parses an identifier
+    * @param value the identifier name as a String
+    * @return an Identifier representing the variable
+    */
   private def parseID(value: String): ExprTree = {
     eat(ID(value))
     val e = Identifier(value)
     e
   }
 
+  /**
+    * Parses a GCD function.
+    * @return a Gcd representing the GCD function.
+    */
   private def parseGCD(): ExprTree = {
+    /** Read the gcd function and the first parenthesis */
     eat(GCD)
     eat(LPAREN)
+    /** First argument **/
     val ret = parsePlusMinus
-    eat(VIRG)
-    val e = Gcd(ret, parsePlusMinus)
+    /** Read the second argument */
+    val e = Gcd(ret, parseVirgule())
+    /** Read the closing parenthesis */
     eat(RPAREN)
     e
   }
 
+  /**
+    * Parses the modular inverse.
+    * @return a InvMod representing the modular inverse
+    */
   private def parseInvmod(): ExprTree = {
+    /** Read the invmod function and the left parenthesis */
     eat(INVMOD)
     eat(LPAREN)
+    /** First argument **/
     val ret = parsePlusMinus
-    eat(VIRG)
-    val e = InvMod(ret, parsePlusMinus)
+    /** Read the second argument */
+    val e = InvMod(ret, parseVirgule())
+    /** Read the closing parenthesis */
     eat(RPAREN)
     e
   }
 
+  /**
+    * Parses a square root function. It the calls the parsePlusMinus
+    * in order to parse any expression inside the square root.
+    * @return a Sqrt object representing the square root
+    */
   private def parseSQRT(): ExprTree = {
     eat(SQRT)
     val e = Sqrt(parsePlusMinus)
     e
   }
 
+  /**
+    * Parses a parenthesis. It the calls the parsePlusMinus
+    * in order to parse any expression inside the parenthesis.
+    * @return a ExprTree representing a calculation inside a parenthesis
+    */
   private def parseParenthesis(): ExprTree = {
     eat(LPAREN)
     val ret = parsePlusMinus
@@ -231,6 +297,10 @@ class Parser(source:Source) extends Lexer(source:Source) {
     ret
   }
 
+  /**
+    * Parses a coma.
+    * @return an ExprTree representing the calculation after a coma.
+    */
   private def parseVirgule(): ExprTree = {
     eat(VIRG)
     parsePlusMinus
